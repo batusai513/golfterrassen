81ffa5db5409f080d70ba7e66fefa642aee43113
Richard Roncancio <batusai513@msn.com>
2017-06-04 19:29:53 -0500

    Moar html and styles

--- js/index.js
+++ js/index.js
@@ -1,69 +1,123 @@
-"use strict"
+"use strict";
 
-const fullpageStyles = require('fullpage.js/dist/jquery.fullpage.min.css')
-const styles = require('../css/style.scss')
-const $ = require('jquery')
-const fullpage = require('fullpage.js')
-const SmartPhone = require('detect-mobile-browser')(false);
+const fullpageStyles = require("fullpage.js/dist/jquery.fullpage.min.css");
+const styles = require("../css/style.scss");
+const $ = require("jquery");
+const fullpage = require("fullpage.js");
+const SmartPhone = require("detect-mobile-browser")(false);
+const magnificPopup = require("magnific-popup");
+const Flickity = require("flickity");
+const Rellax = require("rellax");
+
+const mainMenu = document.querySelector(".js-menu");
+const rellax = new Rellax(".rellax", {
+  center: true
+});
+
+$(".js-gallery").magnificPopup({
+  delegate: "a",
+  type: "image",
+  closeOnContentClick: false,
+  closeBtnInside: false,
+  mainClass: "mfp-with-zoom mfp-img-mobile",
+  image: {
+    verticalFit: true,
+    titleSrc: function(item) {
+      return (
+        item.el.attr("title") +
+        ' &middot; <a class="image-source-link" href="' +
+        item.el.attr("data-source") +
+        '" target="_blank">image source</a>'
+      );
+    }
+  },
+  gallery: {
+    enabled: true
+  },
+  zoom: {
+    enabled: true,
+    duration: 300, // don't foget to change the duration also in CSS
+    opener: function(element) {
+      return element.find("img");
+    }
+  }
+});
 
 const demos = {
-  2: document.querySelector('#search-demo'),
-  3: document.querySelector('#radio-demo'),
-  4: document.querySelector('#collections-demo'),
-}
+  2: document.querySelector("#search-demo"),
+  3: document.querySelector("#radio-demo"),
+  4: document.querySelector("#collections-demo")
+};
 
 const swapVideos = (index, nextIndex) => {
   const previousVideo = demos[index];
   const nextVideo = demos[nextIndex];
 
   if (previousVideo) {
-    previousVideo.pause()
+    previousVideo.pause();
   }
 
   if (nextVideo) {
-    nextVideo.play()
+    nextVideo.play();
   }
-}
+};
 
 const renderHeroBackground = () => {
-  const url = '../images/bg1.jpg'
+  const url = "../images/bg1.jpg";
   const img = new Image();
-  img.onload = function(){
-   $('.overlay').css({'background-image': 'url('+url+')', 'opacity': 1});
-  }
+  img.onload = function() {
+    $(".overlay").css({ "background-image": "url(" + url + ")", opacity: 1 });
+  };
   img.src = url;
-}
+};
 
-const initFullPageJs = () => {
-  $('#fullpage').fullpage({
-    navigation: true,
-    onLeave(index, nextIndex, direction) {
-      swapVideos(index, nextIndex)
-    }
+// const initFullPageJs = () => {
+//   $('#fullpage').fullpage({
+//     navigation: true,
+//     onLeave(index, nextIndex, direction) {
+//       swapVideos(index, nextIndex)
+//     }
+//   });
+
+//   $('.scroll-arrow').click(() => {
+//     $.fn.fullpage.moveSectionDown();
+//   })
+// }
+
+function startMobileMenu(menu) {
+  return new Flickity(menu, {
+    cellAlign: "left",
+    freeScroll: true,
+    prevNextButtons: false,
+    pageDots: false,
+    contain: true,
+    percentPosition: false
   });
-
-  $('.scroll-arrow').click(() => {
-    $.fn.fullpage.moveSectionDown();
-  })
 }
 
 const handleMobileDetection = () => {
+  if (!SmartPhone.isAny()) {
+    startMobileMenu(mainMenu);
+    $(".js-menu-link").on("dragstart", e => e.preventDefault());
+  }
   if (SmartPhone.isAny()) {
     // replace videos with images
-    const demos = ['search-demo','radio-demo','collections-demo'];
-    demos.forEach((demo) => {
-      $(`#${demo}`).replaceWith(`<img src='images/${demo}.png' id='${demo}' class='demo-image'/>`)
-    })
-    $('.download').removeClass('download').html('<p>Available for Windows, Mac and Ubuntu.</p>')
-    $('.demo').click(function() {
-      $(this).toggleClass('active')
-    })
+    const demos = ["search-demo", "radio-demo", "collections-demo"];
+    demos.forEach(demo => {
+      $(`#${demo}`).replaceWith(
+        `<img src='images/${demo}.png' id='${demo}' class='demo-image'/>`
+      );
+    });
+    $(".download")
+      .removeClass("download")
+      .html("<p>Available for Windows, Mac and Ubuntu.</p>");
+    $(".demo").click(function() {
+      $(this).toggleClass("active");
+    });
   }
-
-}
+};
 
 $(document).ready(() => {
-  initFullPageJs()
-  renderHeroBackground()
-  handleMobileDetection()
+  renderHeroBackground();
+  handleMobileDetection();
 });
